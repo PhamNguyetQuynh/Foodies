d005273b4a83e706a582e02c10af6ad14875c238
vyinrealife <129273933+lathuyvy2003@users.noreply.github.com>
2023-10-24 13:19:25 +0700

    by Vy NGo cuti

---
+++ src/adminPanel/adminMessage/adminMessage.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head><meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="../../css/global.css">
+    <link rel="stylesheet" href="../../components/adminHeader/adminHeader.css">
+    <link rel="stylesheet" href="adminMessage.css" />
+    <title>Mesage</title></head>
+    <body>
+    <div class="wrapper">
+            <?php include '../../components/adminHeader/adminHeader.php' ?>
+        </div>
+    <div class="adminMessage">
+          <div class="title">Message</div>
+          <div class="first">
+          <div class="cover">
+          <div class="design">
+          <div class="design1 "> 
+                  <div class="name">John Doe</div>
+                  <div class="purpose">Selling purpose</div>
+                </div>
+                <div class="script">hello!!!</div>
+                <div class="divWrapper">
+                  <div class="group"><div class="btn">Delete Message</div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="group1">
+            <div class="design">
+              <div class="design1">
+                <div class="name">John Doe</div>
+                <div class="purpose">Selling purpose</div>
+              </div>
+              <div class="script">hello!!!</div>
+              <div class="divWrapper">
+                <div class="group"><div class="btn">Delete Message</div></div>
+              </div>
+            </div>
+          </div>
+          <div class="group2">
+            <div class="design">
+              <div class="design1">
+                <div class="name">John Doe</div>
+                <div class="purpose">Selling purpose</div>
+              </div>
+              <div class="script">hello!!!</div>
+              <div class="divWrapper">
+                <div class="group"><div class="btn">Delete Message</div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
