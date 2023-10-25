5b311c37110a5fd23a9de2557ca1f8003ac9bc75
christinechinguyen002 <chinguyenthi056@gmail.com>
2023-10-25 20:22:35 +0700

    laine

---
+++ src/adminPanel/viewProduct/viewProduct.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Product</title>
+    <link rel="stylesheet" href="viewProduct.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Sora' rel='stylesheet'>
+    <link rel="stylesheet" href="global.css">
+</head>
+<body>
+    <div class="heading">Your Product</div>
+    
+    <div class="container">
+        <div class="content-section">
+            <div class="card">
+                <img src="salad.jpeg" alt="salad">
+                <h2>Salad</h2>
+                <button class="button">Edit</button>
+                <button class="button">Delete</button>
+                <button class="button">View post</button>
+            </div>
+
+            <div class="card">
+                <img src="salad.jpeg" alt="salad">
+                <h2>Salad</h2>
+                <button class="button">Edit</button>
+                <button class="button">Delete</button>
+                <button class="button">View post</button>
+            </div>
+
+            <div class="card">
+                <img src="salad.jpeg" alt="salad">
+                <h2>Salad</h2>
+                <button class="button">Edit</button>
+                <button class="button">Delete</button>
+                <button class="button">View post</button>
+            </div>
+            <div class="card">
+                <img src="salad.jpeg" alt="salad">
+                <h2>Salad</h2>
+                <button class="button">Edit</button>
+                <button class="button">Delete</button>
+                <button class="button">View post</button>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
