61af36fd931616dce3ff31b5bf45831f8497037f
christinechinguyen002 <chinguyenthi056@gmail.com>
2023-10-27 13:52:25 +0700

    2710

--- src/adminPanel/viewProduct/viewProduct.php
+++ src/adminPanel/viewProduct/viewProduct.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Product</title>
+    <!-- link css -->
     <link rel="stylesheet" href="viewProduct.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Sora' rel='stylesheet'>
     <link rel="stylesheet" href="../../css/global.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="../../components/adminHeader/adminHeader.css">
+    <link href='https://fonts.googleapis.com/css?family=Sora' rel='stylesheet'>
 </head>
 <body>
     <?php include '../../components/adminHeader/adminHeader.php' ?>
