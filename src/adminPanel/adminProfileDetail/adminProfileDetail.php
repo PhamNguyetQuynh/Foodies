4363250c64e525b1f0ddb8b8d7a980c9e533de6d
catu9423 <21520513@gm.uit.edu.vn>
2023-10-27 21:29:07 +0700

    camtu

--- src/adminPanel/adminProfileDetail/adminProfileDetail.php
+++ src/adminPanel/adminProfileDetail/adminProfileDetail.php
@@ -6,14 +6,14 @@
     <link rel="stylesheet" href="../../css/global.css">
     <link rel="stylesheet" href="../../components/adminHeader/adminHeader.css">
     <link rel="stylesheet" href="../../adminPanel/adminProfileDetail/adminProfileDetail.css">
-    <title>Foodies-Admin Dashboard page</title></head>
+    <title>Foodies-Admin Profile Detail</title></head>
     <body>
         <div class="wrapper">
             <?php include '../../components/adminHeader/adminHeader.php' ?>
         </div>
 
         <b class="ProfileDetail">Profile Details</b>
-        <div class="container">
+        <div class="adminProfileDetailChild ">
         <b class="name">name</b>
         <div class="seller">seller</div>
         <img
@@ -24,23 +24,26 @@
        
 
         <div class="boxProduct">
-            <div class="group-child"></div>
+            <div class="group-child">
             <b class="ProductAdded">Products Added</b>
             <b class="ViewDetail" id="viewDetails">View details &gt;&gt;&gt;</b>
             <b class="b">17</b>
+            </div>
         </div>
         <div class="boxTotalOrder">
-            <div class="group-child"></div>
+            <div class="group-child">
             <b class="TotalOrder">Total Orders</b>
             <b class="ViewDetail1" id="viewDetails1">View details &gt;&gt;&gt;</b>
             <b class="b1">20</b>
+            </div>
         </div>
         <div class="group-div" id="groupContainer4">
-            <div class="group-inner"></div>
+            <div class="group-inner">
             <div class="update-profile" id="updateProfileText">Update profile</div>
+            </div>
         </div>
         </div>
-        </div>
+    
     </body>
       
   
