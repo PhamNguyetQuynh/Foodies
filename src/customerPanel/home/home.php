0628dae6b938b5da770bdc200581605eacb93a17
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-11-01 22:36:11 +0700

    kho du day

--- src/customerPanel/home/home.php
+++ src/customerPanel/home/home.php
@@ -6,15 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="../../css/global.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
     <link rel="stylesheet" href="../../components/customerHeader/customerHeader.css">
     <link rel="stylesheet" href="../../components/customerFooter/customerFooter.css">
+    <link rel="stylesheet" href="../home/home.css">
     <script src="../../js/customerScript.js"></script>
     <title>Foodies-Homepage</title>
 </head>
+
 <body>
-    <?php include '../../components/customerHeader/customerHeader.php'; ?>
-<h1>Homepage</h1>
-    <?php include '../../components/customerFooter/customerFooter.php'; ?>
+    <div class="grid">
+        <div class="headerWrapper">
+            <?php include '../../components/customerHeader/customerHeader.php'; ?>
+        </div>
+
+        <div class="hero">
+            <h1 class="c-o-1 m-o-1 l-o-1">FOODIES</h1>
+            <p class="c-o-1 m-o-1 l-o-1">Nourish your body, indulge your taste buds
+                Welcome to a world of healthy delights!</p>
+        </div>
+        <h1>Homepage</h1>
+        <?php include '../../components/customerFooter/customerFooter.php'; ?>
+    </div>
+
 </body>
 
 </html>
