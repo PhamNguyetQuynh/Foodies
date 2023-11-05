b4fe02a5af00b75d98764fb38341c7a40632663c
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-11-05 11:16:09 +0700

    fix

---
+++ src/customerPanel/wishlist/wishlist.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="../../css/global.css">
+    <link rel="stylesheet" href="../../components/customerHeader/customerHeader.css">
+    <link rel="stylesheet" href="../../components/customerFooter/customerFooter.css">
+    <link rel="stylesheet" href="../../customerPanel/wishlist/wishlist.css">
+    <script src="../../js/customerScript.js"></script>
+    <title>Foodies-Wishlist</title>
+</head>
+
+<body>
+    <section class="headerWrapper">
+        <?php include '../../components/customerHeader/customerHeader.php'; ?>
+    </section>
+
+    <section class="hero">
+    </section>
+    <section class="display">
+       
+            <h1 class="">Wishlist</h1>
+       
+    </section>
+    <?php include '../../components/customerFooter/customerFooter.php'; ?>
+</body>
+
+</html>
