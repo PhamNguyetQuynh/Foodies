385ff40c9172d6cfb0b6a0d027c74975a2a8d606
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-11-05 15:31:48 +0700

    77%

--- src/customerPanel/wishlist/wishlist.php
+++ src/customerPanel/wishlist/wishlist.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="../../css/global.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
     <link rel="stylesheet" href="../../components/customerHeader/customerHeader.css">
     <link rel="stylesheet" href="../../components/customerFooter/customerFooter.css">
     <link rel="stylesheet" href="../../customerPanel/wishlist/wishlist.css">
@@ -20,10 +21,73 @@
 
     <section class="hero">
     </section>
-    <section class="display">
-       
-            <h1 class="">Wishlist</h1>
-       
+    <section class="display grid ">
+        <div class="c-o-2 m-o-2 l-o-2">
+            <h1>Wishlist</h1>
+            <div class="content-section container">
+                <div class="card">
+                    <img class="product-img" src="../../img/salad.svg" alt="salad">
+                    <div class="card-header">
+                        <h2 class="card-title">Salad</h2>
+                        <button class="fav"><i class="fa-solid fa-heart"></i></button>
+                    </div>
+                    <p class="price">20$</p>
+        
+                    <div class="btn-container">
+                        <button class="btn-viewProduct btn">Buy Now</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="product-img" src="../../img/salad.svg" alt="salad">
+                    <div class="card-header">
+                        <h2 class="card-title">Salad</h2>
+                        <button class="fav"><i class="fa-solid fa-heart"></i></button>
+                    </div>
+                    <p class="price">20$</p>
+        
+                    <div class="btn-container">
+                        <button class="btn-viewProduct btn">Buy Now</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="product-img" src="../../img/salad.svg" alt="salad">
+                    <div class="card-header">
+                        <h2 class="card-title">Salad</h2>
+                        <button class="fav"><i class="fa-solid fa-heart"></i></button>
+                    </div>
+                    <p class="price">20$</p>
+        
+                    <div class="btn-container">
+                        <button class="btn-viewProduct btn">Buy Now</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="product-img" src="../../img/salad.svg" alt="salad">
+                    <div class="card-header">
+                        <h2 class="card-title">Salad</h2>
+                        <button class="fav"><i class="fa-solid fa-heart"></i></button>
+                    </div>
+                    <p class="price">20$</p>
+        
+                    <div class="btn-container">
+                        <button class="btn-viewProduct btn">Buy Now</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="product-img" src="../../img/salad.svg" alt="salad">
+                    <div class="card-header">
+                        <h2 class="card-title">Salad</h2>
+                        <button class="fav"><i class="fa-solid fa-heart"></i></button>
+                    </div>
+                    <p class="price">20$</p>
+        
+                    <div class="btn-container">
+                        <button class="btn-viewProduct btn">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </section>
     <?php include '../../components/customerFooter/customerFooter.php'; ?>
 </body>
