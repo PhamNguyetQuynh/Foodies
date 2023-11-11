ad5513ad2b009f375938652ffff1f42615dd234f
LeNguyenDiemQuynh <21521371@gm.uit.edu.vn>
2023-11-11 23:06:46 +0700

    Continue Order

--- src/customerPanel/order/order.php
+++ src/customerPanel/order/order.php
@@ -15,24 +15,21 @@
 <body>
     <?php include '../../components/customerHeader/customerHeader.php'; ?>
     <div class="orderContainer">
-        <div class="background-image"></div>
         <h1>My Order</h1>
-        <table>
-            <tr>
-                <td>
-                    <img src="../../img/imageOrder.svg" alt="Product Image" />
-                </td>
-            </tr>
-            <tr>
-                <td class="product-description">
-                    <p>Salad</p>
-                    <p>$15</p>
-                    <p>A feast for the senses, with fresh ingredients and a flavorful dressing</p>
-                    <p>Trạng thái: In progress</p>
-                </td>
-            </tr>
-        </table>
-    </div>
+        <div class="product-card">
+            <img src="../../img/imageOrder.svg" alt="Product Image" class="product-image">
+            <div class="product-info">
+                <div class="orderRow">
+                    <h3 class="product-name">Salad</h3>
+                    <p class="product-price">$15</p>
+                </div>
+                <p class="product-description">
+                    A feast for the senses, with fresh ingredients and a flavorful dressing
+                </p>
+                <p class="product-status">In Progress</p>
+            </div>
+        </div>
+    </div>    
     <?php include '../../components/customerFooter/customerFooter.php'; ?>
 </body>
 </html>
