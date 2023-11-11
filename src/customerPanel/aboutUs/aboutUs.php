0328672bedb24a20872f70f971ccd0f423585c06
vyinrealife <129273933+lathuyvy2003@users.noreply.github.com>
2023-11-11 15:11:22 +0700

    moi dc 50% hoi

---
+++ src/customerPanel/aboutUs/aboutUs.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <link rel="stylesheet" href="../../css/global.css">
+  <link rel="stylesheet" href="../../components/customerHeader/customerHeader.css">
+  <link rel="stylesheet" href="../../components/customerFooter/customerFooter.css">
+  <link rel="stylesheet" href="../../customerPanel/aboutUs/aboutUs.css">
+  <script src="../../js/customerScript.js"></script>
+  <title>Foodies</title>
+</head>
+    <body>
+    <div class="wrapper"> <?php include '../../components/customerHeader/customerHeader.php' ?></div>
+    <div class="aboutUs">
+            <div class="background"><img class="image" src="../../img/bg.svg" /></div>
+                <div class="total">
+                    <div class="titleAbout">About us</div>
+                    <p class="contentAbout">
+                        Nourish your body, indulge your taste buds<br />Welcome to a world of healthy delights!
+                    </p>
+                </div>
+           
+            <div class="separate">
+                <img class="image" src="../../img/img.svg" />
+            </div>
+            
+            <div class="masterchefPage">
+                <img class="image" src="../../img/bg2.svg" />
+                <div class="masterchef">Masterchef</div>
+                <div class="contentMasterchef">
+                    <div class="a">Chef Antonio was born and raised in the small town of Amalfi, Italy, where he grew up surrounded by the
+                            fresh seafood and seasonal produce that have become the hallmarks of his cuisine. His passion for cooking
+                            began at a young age, when he would spend hours in his grandmother's kitchen learning the secrets of
+                            traditional Italian recipes. Chef Antonio is passionate about creating a warm and welcoming dining
+                            experience for his guests. He invites you to come and taste the authentic flavors of Amalfi at his
+                            restaurant, where he will take you on a culinary journey through the heart of Southern Italy.</div>
+                            <br></br>
+                    <div class="b">Explore our website today and experience the magic of Chef Antonio's cuisine!
+                </div>
+                <div class="selection">
+                    <div class="btn btnExplore"><a href="../editProduct/editProduct.php">Explore Our Menu</a></div>
+                    <div class="btn btnVisit"><a href="../editProduct/editProduct.php">Visit Our Shop</a></div>
+                </div>
+            </div>
+
+            <div class="ourStoryPage">
+                <div class="titleOur">Our Story</div>
+                <div class="contentOur">
+                    <div class="c">Our story began with a love of family, food, and the Italian way of life. We source our ingredients from
+                            local farmers and markets whenever possible, and we take pride in preparing our dishes with care and
+                            attention to detail.<br/>We believe that food is more than just sustenance. It is a way to bring people
+                            together and to celebrate life. We invite you to come and experience the magic of Italian dining at our
+                            restaurant. We will welcome you with open arms and treat you like family.<br /><br></div>
+                    <div class="d">Buon appetito!</div>
+                </div>
+                <div class="btn btnService"><a href="../editProduct/editProduct.php">Our Service</a></div>
+            </div>
+
+
+        
+        </div> 
+        <div class="wrapper"> <?php include '../../components/customerFooter/customerFooter.php' ?></div>
+    </body>
+   
+</html>
+
