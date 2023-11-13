cdbd48a2e201f14b19dbe1149c10e7a147467ead
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-11-13 15:40:57 +0700

    tui sua mot ti ma van can sua

--- src/customerPanel/productDetail/productDetail.php
+++ src/customerPanel/productDetail/productDetail.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/global.css">
     <link rel="stylesheet" href="../../components/customerHeader/customerHeader.css">
+    <link rel="stylesheet" href="../../components/customerFooter/customerFooter.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script src="../../js/customerScript.js"></script>
     <link rel="stylesheet" href="productDetail.css">
     <title>Product detail</title>
 </head>
@@ -22,11 +25,12 @@
                 <p class="prd-price">$40.0</p>
                 <p class="prd-description">French fries are a beloved and iconic fast food dish that needs no introduction. They are made from potatoes, typically Russet or Idaho varieties, which are cut into long, thin strips and then deep-fried to achieve a crispy, golden exterior and a fluffy, tender interior.</p>
                 <div class="btn-group">
-                    <button class="prd-btn">Add to Wishlist</button>
-                    <button class="prd-btn">Add to Cart</button>
+                    <button class="prd-btn btn">Add to Wishlist</button>
+                    <button class="prd-btn btn">Add to Cart</button>
                 </div>
             </div>
         </div>
     </div>
+    <?php include '../../components/customerFooter/customerFooter.php'; ?>
 </body>
 </html>
