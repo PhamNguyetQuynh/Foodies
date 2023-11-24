fae031197bf3eff7d68d9521f2fa2a421e84abef
catu9423 <21520513@gm.uit.edu.vn>
2023-11-24 11:26:20 +0700

    camtu

--- FoodiesBistro&Garden/includes/header.php
+++ FoodiesBistro&Garden/includes/header.php
@@ -19,6 +19,7 @@
     <!-- alertify js -->
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Foodies Bistro & Garden</title>
 </head>
--- FoodiesBistro&Garden/includes/navbar.php
+++ FoodiesBistro&Garden/includes/navbar.php
@@ -1,47 +1,71 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">Foodies</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="category.php">Collections</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="cart.php">Cart</a>
-        </li>
-        <?php
-        if (isset($_SESSION['auth'])) {
-        ?>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <?= $_SESSION['auth_user']['name'];   ?>
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="myOrder.php">My Orders</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-            </ul>
-          </li>
-        <?php
-        } else {
-        ?>
-          <li class="nav-item">
-            <a class="nav-link" href="register.php">Register</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.php">Login</a>
-          </li>
+  <div class="container py-2 px-4">
+    <div class="row">
+      <div class="col-md-4">
+        <a class="navbar-brand fs-4" href="index.php" style="font-weight: bolder; ">Foodies</a>
+      </div>
 
-        <?php
-        }
-        ?>
-      </ul>
+      <div class="col-md-4">
+        <form class="d-flex justify-content-between">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search" style="width: 300px">
+          <button class="btn" type="submit" style="background-color: white;">
+            <i class="fa fa-search" style="color: #7D6323;"></i>
+          </button>
+        </form>
+      </div>
+
+      <div class="col-md-3">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="width:95px;" href="aboutUs.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="width:100px;" href="category.php">Our Menu </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="wishlist.php">Wishlist</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cart.php">Cart</a>
+            </li>
+
+          </ul>
+
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+              <?php
+              if (isset($_SESSION['auth'])) {
+              ?>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <?= $_SESSION['auth_user']['name'];   ?>
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="myOrder.php">My Orders</a></li>
+                    <li><a class="dropdown-item" href="myProfile.php">My Profile</a></li>
+                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                  </ul>
+                </li>
+              <?php
+              } else {
+              ?>
+                <li class="nav-item">
+                  <a class="nav-link" href="register.php">Register</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="login.php">Login</a>
+                </li>
+              <?php
+              }
+              ?>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </nav>
