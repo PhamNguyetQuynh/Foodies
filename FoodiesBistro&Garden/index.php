6c09dc7b725180b124af42bcdd882773e3f3e2d8
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-11-26 20:36:14 +0700

    sua footer

--- FoodiesBistro&Garden/index.php
+++ FoodiesBistro&Garden/index.php
@@ -55,28 +55,7 @@ include('./includes/slider.php'); ?>
         </div>
     </div>
 </div>
-<div class="py-5 bg-dark">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <h4 class="text-white">Foodies Bistro & Garden</h4>
-                <div class="underline mb-2"> </div>
-                <a href="index.php" class="text-white">Home</a> <br>
-                <a href="#" class="text-white">About Us</a><br>
-                <a href="category.php" class="text-white">Our Menu</a> <br>
-            </div>
-            <div class="col-md-4">
-                <h4 class="text-white">Address</h4>
-                <p class="text-white">42 Nguyen Hue Street, District 1, Ho Chi Minh City, Viet Nam</p>
-                <a href="tel: +84989598472" class="text-white"><i class="fa fa-phone"></i> +(84) 98 95 98 472</a><br>
-                <a href="mailto:xyz@gmail.com" class="text-white"><i class="fa fa-envelope"></i> foddiesbistroandgarden@gmail.com</a>
-            </div>
-            <div class="col-md-5">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.547984513965!2d106.69074697405321!3d10.76927748937904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fc8702ecc0b%3A0x4e830dc2c0483582!2sDaybyDay%20-%20Kitchen%20%26%20Bar!5e0!3m2!1svi!2s!4v1700565681506!5m2!1svi!2s" class="w-100" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-        </div>
-    </div>
-</div>
+
 <div class="py-2 bg-danger">
     <div class="text-end">
         <div class="mb-0 me-3 text-white">All rights reserved. Copy right @Foodies Bistro & Garden - <?= date('Y'); ?> </div>
