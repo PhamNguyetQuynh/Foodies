cdf0748251b0a645eb8739edc07e7f35a991c29e
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-12-07 21:36:36 +0700

    delete unessary thing

--- old/src/js/customerScript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const userIcon = document.querySelector('#userIcon');
-    const userHoverBox = document.querySelector('.accountHover');
-
-    userIcon.addEventListener('click', function () {
-        userHoverBox.classList.toggle('active');
-    });
-});
