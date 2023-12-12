7d45c73451e1e76d5469e9a507fc2ebe33d4abb0
catu9423 <21520513@gm.uit.edu.vn>
2023-12-12 20:45:31 +0700

    thêm với chỉnh

--- FoodiesBistro&Garden/includes/navbar.php
+++ FoodiesBistro&Garden/includes/navbar.php
@@ -1,31 +1,36 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
   <div class="container py-2 px-4">
     <div class="row">
-      <div class="col-md-4">
+    <div class="col-md-2">
         <a class="navbar-brand fs-4" href="index.php" style="font-weight: bolder; ">Foodies</a>
         <button class="navbar-toggler float-end " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       </div>
 
-      <div class="col-md-4">
-        <form class="d-flex justify-content-between">
-          <input class="form-control ml-auto" type="search" placeholder="Search" aria-label="Search" style="width: 300px">
-          <button class="btn" type="submit" style="background-color: white;">
+      <div class="col-md-2">
+        <form id="searchForm" action="product.php?" method="GET" class="d-flex justify-content-between">
+          <input class="form-control" type="text" id="search" name="key" placeholder="Search for" aria-label="Search" style="width: 85%">
+          <button class="btn" type="submit" title="Search" style="background-color: white;">
             <i class="fa fa-search" style="color: #7D6323;"></i>
           </button>
         </form>
-
       </div>
 
-      <div class="col-md-3">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
+      <div class="col-md-8">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+          <li class="nav-item">
               <a class="nav-link <?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'active' : ''; ?>" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link <?php echo (basename($_SERVER['PHP_SELF']) == 'aboutUs.php') ? 'active' : ''; ?>" style="width:95px;" href="aboutUs.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link <?php echo (basename($_SERVER['PHP_SELF']) == 'contactUs.php') ? 'active' : ''; ?>" style="width:110px;" href="contactUs.php">Contact Us</a> <!-- Check ref -->
+            </li>
+            <li class="nav-item">
+              <a class="nav-link <?php echo (basename($_SERVER['PHP_SELF']) == 'contactUs.php') ? 'active' : ''; ?>" style="width:110px;" href="contactUs.php">Reservation</a> <!-- Thêm ref -->
             </li>
             <li class="nav-item">
               <a class="nav-link <?php echo (basename($_SERVER['PHP_SELF']) == 'category.php') ? 'active' : ''; ?>" style="width:100px;" href="category.php">Our Menu </a>
@@ -38,7 +43,7 @@
             </li>
           </ul>
 
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          
             <ul class="navbar-nav ms-auto">
               <?php
               if (isset($_SESSION['auth'])) {
@@ -66,7 +71,7 @@
               }
               ?>
             </ul>
-          </div>
+          
         </div>
       </div>
     </div>
