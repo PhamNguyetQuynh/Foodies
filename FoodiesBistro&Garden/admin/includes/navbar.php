35bcd3f1ab920e9e0723ce0b0683710f61bb6b92
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-12-15 01:06:38 +0700

    delete search bar and unnecessary things

--- FoodiesBistro&Garden/admin/includes/navbar.php
+++ FoodiesBistro&Garden/admin/includes/navbar.php
@@ -3,9 +3,7 @@
 
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                <div class="input-group input-group-outline">
-                    <input type="text" class="form-control" placeholder="Type here...">
-                </div>
+                
             </div>
         </div>
     </div>
