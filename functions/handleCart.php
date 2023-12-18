ec50800d045319f3e3e6e26890e4e4d8b19f052e
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-12-18 07:17:18 +0700

    hot fix

--- functions/handleCart.php
+++ functions/handleCart.php
@@ -14,26 +14,14 @@ if (isset($_SESSION['auth'])) {
                     $product_qty = $_POST['product_qty'];
 
                     $user_id = $_SESSION['auth_user']['user_id'];
+                    $check_existing = "SELECT * FROM carts WHERE product_id='$product_id' AND user_id='$user_id'";
+                    $check_existing_run = mysqli_query($conn, $check_existing);
 
-                    // Use Prepared Statements to prevent SQL injection
-                    // $check_existing = "SELECT * FROM carts WHERE product_id='$product_id' AND user_id='$user_id'";
-                    // $check_existing_run = mysqli_query($conn, $check_existing);
-                    $check_existing_query = "SELECT * FROM carts WHERE product_id=? AND user_id=?";
-                    $check_existing_stmt = $conn->prepare($check_existing_query);
-                    $check_existing_stmt->bind_param('ii', $product_id, $user_id);
-                    $check_existing_stmt->execute();
-                    $check_existing_result = $check_existing_stmt->get_result();
-
-                    if ($check_existing_result->num_rows > 0) {
+                    if (mysqli_num_rows($check_existing_run) > 0) {
                         echo "existing";
                     } else {
-                        // Use Prepared Statements to prevent SQL injection
-                        // $insert_query = "INSERT INTO carts(user_id, product_id, product_qty) VALUES ('$user_id','$product_id','$product_qty')";
-                        // $insert_query_run = mysqli_query($conn, $insert_query);
-                        $insert_query = "INSERT INTO carts(user_id, product_id, product_qty) VALUES (?, ?, ?)";
-                        $insert_stmt = $conn->prepare($insert_query);
-                        $insert_stmt->bind_param('iii', $user_id, $product_id, $product_qty);
-                        $insert_query_run = $insert_stmt->execute();
+                        $insert_query = "INSERT INTO carts(user_id, product_id, product_qty) VALUES ('$user_id','$product_id','$product_qty')";
+                        $insert_query_run = mysqli_query($conn, $insert_query);
 
                         if ($insert_query_run) {
                             echo 201;
@@ -49,21 +37,12 @@ if (isset($_SESSION['auth'])) {
 
                     $user_id = $_SESSION['auth_user']['user_id'];
 
-                    // $check_existing = "SELECT * FROM carts WHERE product_id='$product_id' AND user_id='$user_id'";
-                    // $check_existing_run = mysqli_query($conn, $check_existing);
-                    $check_existing_query = "SELECT * FROM carts WHERE product_id=? AND user_id=?";
-                    $check_existing_stmt = $conn->prepare($check_existing_query);
-                    $check_existing_stmt->bind_param('ii', $product_id, $user_id);
-                    $check_existing_stmt->execute();
-                    $check_existing_result = $check_existing_stmt->get_result();
+                    $check_existing = "SELECT * FROM carts WHERE product_id='$product_id' AND user_id='$user_id'";
+                    $check_existing_run = mysqli_query($conn, $check_existing);
 
-                    if ($check_existing_result->num_rows > 0) {
-                        // $update_query = "UPDATE carts SET product_qty='$product_qty' WHERE product_id='$product_id' AND user_id='$user_id'";
-                        // $update_query_run = mysqli_query($conn, $update_query);
-                        $update_query = "UPDATE carts SET product_qty=? WHERE product_id=? AND user_id=?";
-                        $update_stmt = $conn->prepare($update_query);
-                        $update_stmt->bind_param('iii', $product_qty, $product_id, $user_id);
-                        $update_query_run = $update_stmt->execute();
+                    if (mysqli_num_rows($check_existing_run) > 0) {
+                        $update_query = "UPDATE carts SET product_qty='$product_qty' WHERE product_id='$product_id' AND user_id='$user_id'";
+                        $update_query_run = mysqli_query($conn, $update_query);
                         if ($update_query_run) {
                             echo 200;
                         } else {
@@ -78,23 +57,11 @@ if (isset($_SESSION['auth'])) {
 
                     $user_id = $_SESSION['auth_user']['user_id'];
 
-                    // $check_existing = "SELECT * FROM carts WHERE id='$cart_id' AND user_id='$user_id'";
-                    // $check_existing_run = mysqli_query($conn, $check_existing);
-
-                    $check_existing_query = "SELECT * FROM carts WHERE id=? AND user_id=?";
-                    $check_existing_stmt = $conn->prepare($check_existing_query);
-                    $check_existing_stmt->bind_param('ii', $cart_id, $user_id);
-                    $check_existing_stmt->execute();
-                    $check_existing_result = $check_existing_stmt->get_result();
-
-                    if ($check_existing_result->num_rows > 0) {
-                        // $delete_query = "DELETE FROM carts WHERE id='$cart_id'";
-                        // $delete_query_run = mysqli_query($conn, $delete_query);
-                        $delete_query = "DELETE FROM carts WHERE id=?";
-                        $delete_stmt = $conn->prepare($delete_query);
-                        $delete_stmt->bind_param('i', $cart_id);
-                        $delete_query_run = $delete_stmt->execute();
-
+                    $check_existing = "SELECT * FROM carts WHERE id='$cart_id' AND user_id='$user_id'";
+                    $check_existing_run = mysqli_query($conn, $check_existing);
+                    if (mysqli_num_rows($check_existing_run) > 0) {
+                        $delete_query = "DELETE FROM carts WHERE id='$cart_id'";
+                        $delete_query_run = mysqli_query($conn, $delete_query);
                         if ($delete_query_run) {
                             echo 200;
                         } else {
@@ -107,8 +74,10 @@ if (isset($_SESSION['auth'])) {
                 default:
                     echo 500;
             }
-        }   
+        }
+    } else {
+        echo 403; // Forbidden: Admins are not allowed to perform cart actions
+    }
 } else {
-    echo 403;
-}
+    echo 401;
 }
